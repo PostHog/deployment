59c74aaba19d5a5a6e23e701af0c436deeef7a7c
James Greenhill <jams@uber.com>
2020-06-02 20:36:01 -0700

    Packer config for building AWS EC2 AMI for marketplace

---
+++ packer/single_node/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt -y install apt-transport-https ca-certificates curl git gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/PostHog/deployment.git
+cd deployment/terraform/digitalocean/single_node
+sudo docker-compose -f docker-compose.do.yml up -d
