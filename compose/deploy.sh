e2a9742d7b73c89d023cc6f5376dd617f1e7ee9c
James Greenhill <fuziontech@gmail.com>
2021-11-11 14:07:59 -0800

    a little heavy handed with the -E

--- compose/deploy.sh
+++ compose/deploy.sh
@@ -16,18 +16,18 @@ echo "Do you have a Sentry DSN you would like for debugging should something go 
 echo "If you do enter it now, otherwise just hit enter to continue"
 read SENTRY_DSN
 export SENTRY_DSN=$SENTRY_DSN
-echo "We will need sudo -E access so the next question is for you to give us superuser access"
-sudo -E echo ""
+echo "We will need sudo access so the next question is for you to give us superuser access"
+sudo echo ""
 echo "Thanks! 🙏"
 echo "Ok! We'll take it from here 🚀"
 
 # update apt cache
 echo "Grabbing latest apt caches"
-sudo -E apt update
+sudo apt update
 
 # clone posthog
 echo "Installing PostHog 🦔 from Github"
-sudo -E apt install -y git
+sudo apt install -y git
 rm -rf posthog
 git clone https://github.com/PostHog/posthog.git 
 
@@ -41,18 +41,18 @@ EOF
 
 # setup docker
 echo "Setting up Docker"
-sudo -E apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -E apt-key add -
-sudo -E add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo -E apt update
-sudo -E apt-cache policy docker-ce
-sudo -E apt install -y docker-ce
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt install -y docker-ce
 
 
 # setup docker-compose
 echo "Setting up Docker Compose"
-sudo -E curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose || true
-sudo -E chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose || true
+sudo chmod +x /usr/local/bin/docker-compose
 
 # start up the stack (remember to have --build here for upgrades)
 rm -f docker-compose.yml
