51b56ecc6f0d658a3739523efa7755417ce3cb81
James Greenhill <fuziontech@gmail.com>
2021-11-11 13:32:39 -0800

    wrap things up for stack running

--- compose/deploy.sh
+++ compose/deploy.sh
@@ -7,6 +7,7 @@ export POSTHOG_SECRET=`echo $RANDOM | md5sum | head -c 25`
 # Talk to the user
 echo "Welcome to the single instance PostHog installer 🦔"
 echo "Let's first start by getting the exact domain PostHog will be installed on"
+echo "Make sure that you have a Host A DNS record pointing to this instance!"
 echo "ie: test.posthog.net"
 read DOMAIN
 export DOMAIN=$DOMAIN
@@ -53,4 +54,4 @@ chmod +x /usr/local/bin/docker-compose
 # start up the stack (remember to have --build here for upgrades)
 rm -f docker-compose.yml
 curl -o docker-compose.yml https://raw.githubusercontent.com/posthog/deployment/HEAD/compose/docker-compose.yml
-docker-compose -f docker-compose.yml up
+docker-compose -f docker-compose.yml up --build -d
